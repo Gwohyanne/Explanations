3c7a2350e74655382693eee11f578a163a3849b4
DUMONT Joanne <joanne.dumont@imt-atlantique.net>
2021-11-09 16:14:19 +0100

    Add comments

--- explanations.ml
+++ explanations.ml
@@ -1,8 +1,6 @@
 (*Author: Joanne Dumont*)
 (*Based on Arthur Gontier's work*)
 
-(*TODO: add print statements*)
-
 (*I - Prerequisites*)
 
 (*I.1 - Definitions*)
@@ -19,7 +17,7 @@ type ind_modifs = | Set of ind_name*ind_symbols*ind_set (*I∈D*)
                   | Addcst of ind_name*ind_name*ind_symbols*ind_const*ind_name (*I3=I2-C I*) 
                   | EXFORALL of ind_name 
                   | EXEXISTS of ind_name 
-type consistancy = AC | BC 
+type consistancy = AC | BC (*AC: equal/non-equal; BC: greater (or equal)/smaller*)
 (*Event types*) 
 type index = Ind of ind_name*ind_modifs list
 type event = | Global_event of bool*var_name*index list*consistancy
@@ -116,28 +114,47 @@ let iprim i = Ind (prim (ind_name i), ind_modifs_list i)
 let iprimneqi i = Ind (prim (ind_name i), Rel (prim (ind_name i),NEQ,ind_name i)::ind_modifs_list i) 
 let addint i sym int = Ind (prim (ind_name i), [Addint (prim (ind_name i), ind_name i, sym, int)]@ind_modifs_list i) 
 let addcst i sym cst i2 = Ind (prim (ind_name i), [Addcst (prim (ind_name i), ind_name i, sym, cst, ind_name i2)]@ind_modifs_list i) 
-let rec uniqueset i set iml = match iml with [] -> [Set (i,IN,set)] | Set (j,_,d)::tl -> if i!=j||set!=d then uniqueset i set tl else [] | _ -> []
+let rec uniqueset i set iml = match iml with
+  | [] -> [Set (i,IN,set)]
+  | Set (j,_,d)::tl -> if i!=j||set!=d then uniqueset i set tl else []
+  | _ -> []
 let sum i d = Ind (prim (ind_name i), [EXFORALL (prim (ind_name i)); Set (prim (ind_name i),IN,d); Rel (prim (ind_name i), NEQ, ind_name i)]@ind_modifs_list i) 
 (*Index finders applyers and removals*)
-let rec iii il = match il with [] -> failwith "Index i has left the building" | i::tl -> match i with Ind (I _,_) -> i | _ -> iii tl
-let rec ttt il = match il with [] -> failwith "Index t has left the building" | i::tl -> match i with Ind (T _,_) -> i | _ -> ttt tl
-let rec ppp il = match il with [] -> failwith "Index p has left the building" | i::tl -> match i with Ind (P _,_) -> i | _ -> ppp tl
-let rec iap f il = match il with [] -> [] | i::tl -> match i with Ind (I _,_) -> f i::iap f tl | _ -> i::iap f tl
+(*TODO: fix repetitions*)
+(*Find index in list*)
+let rec iii il = match il with
+  | [] -> failwith "Index i has left the building"
+  | i::tl -> match i with Ind (I _,_) -> i
+  | _ -> iii tl
+let rec ttt il = match il with
+  | [] -> failwith "Index t has left the building"
+  | i::tl -> match i with Ind (T _,_) -> i
+  | _ -> ttt tl
+let rec ppp il = match il with
+  | [] -> failwith "Index p has left the building"
+  | i::tl -> match i with Ind (P _,_) -> i
+  | _ -> ppp tl
+(*Applies function to one index in a list*)
+let rec iap f il = match il with [] -> [] | i::tl -> match i with | Ind (I _,_) -> f i::iap f tl | _ -> i::iap f tl
 let rec tap f il = match il with [] -> [] | i::tl -> match i with Ind (T _,_) -> f i::tap f tl | _ -> i::tap f tl
 let rec pap f il = match il with [] -> [] | i::tl -> match i with Ind (P _,_) -> f i::pap f tl | _ -> i::pap f tl
+(*Removes index from list*)
 let rec i_out il = match il with [] -> [] | i::tl -> match i with Ind (I _,_) -> tl | _ -> i::i_out tl
 let rec t_out il = match il with [] -> [] | i::tl -> match i with Ind (T _,_) -> tl | _ -> i::t_out tl
 let rec p_out il = match il with [] -> [] | i::tl -> match i with Ind (P _,_) -> tl | _ -> i::p_out tl
+(*Creates an index - used when an event is appplied to several variables*)
 let oni il = Ind (I 1,Set (I 1,IN,D 1)::[])::[]
 let ont il = Ind (T 1,Set (T 1,IN,D 2)::[])::[]
 let onp il = Ind (P 1,Set (P 1,IN,D 3)::[])::[]
 let onr il = Ind (R 1,Set (R 1,IN,D 4)::[])::[]
+(*Creates a function that creates an index*)
 let oniin set = fun il -> Ind (I 1,Set (I 1,IN,set)::[])::[]
 let ontin set = fun il -> Ind (T 1,Set (T 1,IN,set)::[])::[]
 let onpin set = fun il -> Ind (P 1,Set (P 1,IN,set)::[])::[]
 let onrin set = fun il -> Ind (R 1,Set (R 1,IN,set)::[])::[]
 
 (*Index modification functions*)
+(*TODO: fix repetitions*)
 let sumiin set = iap (function x -> sum x set) 
 let sumtin set = tap (function x -> sum x set) 
 let sumpin set = pap (function x -> sum x set) 
@@ -151,13 +168,17 @@ let foralli = function il -> Ind (I 1,[EXFORALL (I 1);Set (I 1,IN,D 1)])::il
 let forallt = function il -> Ind (T 1,[EXFORALL (T 1);Set (T 1,IN,D 2)])::il 
 let forallp = function il -> Ind (P 1,[EXFORALL (P 1);Set (P 1,IN,D 3)])::il 
 let iplus int = iap (function x -> addint x PLUS int)
-let imoin int = iap (function x -> addint x MINUS int)
+let iminus int = iap (function x -> addint x MINUS int)
 let tplus int = tap (function x -> addint x PLUS int)
-let tmoin int = tap (function x -> addint x MINUS int)
+let tminus int = tap (function x -> addint x MINUS int)
 let tplusci c = function il -> tap (function x -> addcst x PLUS c (iii il)) il
-let tmoinci c = function il -> tap (function x -> addcst x MINUS c (iii il)) il
+let tminusci c = function il -> tap (function x -> addcst x MINUS c (iii il)) il
 let tprimin d = tap (function x -> Ind (prim (ind_name x), [Set (prim (ind_name x),IN,d); Rel (prim (ind_name x), NEQ, ind_name x)]@ind_modifs_list x))
-let rec imap l = match l with []->failwith "empty im list" | f::[] -> (fun il -> f il) | f::tl -> (fun il -> f ((imap tl) il))
+(*Creates a function that applies multiple functions*)
+let rec agg_functions l = match l with
+  | []->failwith "empty im list"
+  | f::[] -> (fun il -> f il)
+  | f::tl -> (fun il -> f ((agg_functions tl) il))
 
 (*II.2 - To use the index modification functions*)
 
@@ -439,17 +460,17 @@ let alleq  = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, BC); Reifi
 let alldiff= [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
               Decomp (2, rule5, [Decomp_devent (true , (B 1), id, oni)])]
 let cumul  = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, BC); Reified_devent (true, (B 1), id, id)]);
-              Decomp (2, rule3, [Decomp_devent (true , (B 1), tplusci (C 1), tmoinci (C 1)); Decomp_devent (false, (B 1), id, id); Reified_devent (true, (B 2), id, id)]);
+              Decomp (2, rule3, [Decomp_devent (true , (B 1), tplusci (C 1), tminusci (C 1)); Decomp_devent (false, (B 1), id, id); Reified_devent (true, (B 2), id, id)]);
               Decomp (3, rule5, [Decomp_devent (true , (B 2), id, oni)])]
 let gcc    = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
               Decomp (2, rule7, [Decomp_devent (true , (B 1), id, oni)])]
 let gccn   = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
-              Decomp (2, rule6, [Decomp_devent (true , (B 1), id, oni); Reified_devent (true, (B 2), imap [foralli;p_out], imap [i_out;forallp])]);
+              Decomp (2, rule6, [Decomp_devent (true , (B 1), id, oni); Reified_devent (true, (B 2), agg_functions [foralli;p_out], agg_functions [i_out;forallp])]);
               Decomp (3, rule1, [Global_devent (true ,  O   , id, id, BC); Reified_devent (true, (B 2), id, id)])]
 let incr   = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, BC); Reified_devent (true, (B 1), id, id)]);
-              Decomp (2, rule4, [Decomp_devent (false, (B 1), id, id); Decomp_devent (true , (B 1), imoin 1, iplus 1)])]
+              Decomp (2, rule4, [Decomp_devent (false, (B 1), id, id); Decomp_devent (true , (B 1), iminus 1, iplus 1)])]
 let decr   = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, BC); Reified_devent (true, (B 1), id, id)]);
-              Decomp (2, rule4, [Decomp_devent (true , (B 1), id, id); Decomp_devent (false , (B 1), imoin 1, iplus 1)])]
+              Decomp (2, rule4, [Decomp_devent (true , (B 1), id, id); Decomp_devent (false , (B 1), iminus 1, iplus 1)])]
 let elem   = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
               Decomp (2, rule1, [Global_devent (true ,  I   , id, id, AC); Reified_devent (true, (B 2), id, id)]);
               Decomp (3, rule1, [Global_devent (true ,  V   , id, id, AC); Reified_devent (true, (B 3), id, id)]);
@@ -458,20 +479,20 @@ let elem   = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reifi
 let nvalues= [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
               Decomp (1, rule1, [Global_devent (true ,  N   , id, id, AC); Reified_devent (true, (B 4), id, id)]);
               Decomp (2, rule4, [Decomp_devent (true , (B 1), id, oni); Reified_devent (true, (B 2), id, i_out)]);
-              Decomp (3, rule7, [Decomp_devent (true , (B 2), id, ont); Reified_devent (true, (B 4), imap [foralli;p_out], imap [i_out;t_out;forallp])])]
+              Decomp (3, rule7, [Decomp_devent (true , (B 2), id, ont); Reified_devent (true, (B 4), agg_functions [foralli;p_out], agg_functions [i_out;t_out;forallp])])]
 let atleastnvalues = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
                       Decomp (1, rule1, [Global_devent (true ,  N   , id, id, BC); Reified_devent (true, (B 4), id, id)]);
                       Decomp (2, rule4, [Decomp_devent (true , (B 1), id, oni); Reified_devent (true, (B 2), foralli, i_out)]);
-                      Decomp (3, rule6, [Decomp_devent (true , (B 2), id, ont); Reified_devent (true, (B 4), imap [foralli;p_out], imap [i_out;t_out;forallp])])]
+                      Decomp (3, rule6, [Decomp_devent (true , (B 2), id, ont); Reified_devent (true, (B 4), agg_functions [foralli;p_out], agg_functions [i_out;t_out;forallp])])]
 let atmostnvalues  = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
                       Decomp (1, rule1, [Global_devent (true ,  N   , id, id, BC); Reified_devent (true, (B 4), id, id)]);
                       Decomp (2, rule4, [Decomp_devent (true , (B 1), id, oni); Reified_devent (true, (B 2), foralli, i_out)]);
-                      Decomp (3, rule5, [Decomp_devent (true , (B 2), id, ont); Reified_devent (false, (B 4), imap [foralli;p_out], imap [i_out;t_out;forallp])])]
+                      Decomp (3, rule5, [Decomp_devent (true , (B 2), id, ont); Reified_devent (false, (B 4), agg_functions [foralli;p_out], agg_functions [i_out;t_out;forallp])])]
 let among  = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
               Decomp (2, rule4, [Decomp_devent (true , (B 1), id, ontin (D 4)); Reified_devent (true, (B 2), id, t_out)]);
               Decomp (3, rule7, [Decomp_devent (true , (B 2), id, oni)])]
 let regular= [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
-              Decomp (2, rule4, [Decomp_devent (true , (B 1), id, id);Decomp_devent (false, (B 1), imap [imoin 1;tprimin (D 8)], imap [iplus 1;tprimin (D 9)])])]
+              Decomp (2, rule4, [Decomp_devent (true , (B 1), id, id);Decomp_devent (false, (B 1), agg_functions [iminus 1;tprimin (D 8)], agg_functions [iplus 1;tprimin (D 9)])])]
 let roots  = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
               Decomp (2, rule7, [Decomp_devent (true , (B 1), id, ontin (D 5))]);
               Decomp (2, rule7, [Decomp_devent (true , (B 1), id, ontin (D 6))])]
@@ -479,9 +500,11 @@ let range  = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reifi
               Decomp (2, rule6, [Decomp_devent (true , (B 1), id, ontin (D 5))]);
               Decomp (2, rule7, [Decomp_devent (true , (B 1), id, ontin (D 6))])]
 let table  = [Decomp (1, rule1, [Global_devent (true ,  X   , id, id, AC); Reified_devent (true, (B 1), id, id)]);
-              Decomp (2, rule3, [Decomp_devent (true , (B 1), id, oni); Reified_devent (true, (B 2), id, imap [i_out])]);
+              Decomp (2, rule3, [Decomp_devent (true , (B 1), id, oni); Reified_devent (true, (B 2), id, agg_functions [i_out])]);
               Decomp (4, rule4, [Decomp_devent (true , (B 2), id, onr)])]
 
+(*TODO: change names*)
+(*Global_event(boolean, variable name, list of (index, changes to the index), consistancy)*)
 let xbc = Global_event (true, X, [Ind (I 1, []); Ind (T 1, [])], BC)
 let xac = Global_event (true, X, [Ind (I 1, []); Ind (T 1, [])], AC)
 let nbc = Global_event (true, N, [Ind (P 1, [])], BC)
